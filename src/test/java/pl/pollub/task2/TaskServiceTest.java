b2dc101161bcd8f0d37a3171b784ceea36741662
igor-kopec <igokop99@gmail.com>
2017-08-18 15:09:27 +0200

    podstawa na warsztaty z wzorcow

---
+++ src/test/java/pl/pollub/task2/TaskServiceTest.java
@@ -0,0 +1,48 @@
+package pl.pollub.task2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TaskServiceTest {
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    EmailNotifier emailNotifier;
+
+    @InjectMocks
+    TaskService taskService;
+
+    @Captor
+    private ArgumentCaptor<List<String>> emailsCaptor;
+
+    @Test
+    public void sendEmailToOwnerAndContributors() {
+        Task task = taskService.createTaskForUser(1, 2, 3);
+
+        Mockito.when(userService.getUserById(Mockito.anyInt())).then(invocationOnMock -> {
+            Integer id = invocationOnMock.getArgumentAt(0, int.class);
+            return new User(id, "user" + id + "@wp.pl");
+        });
+
+        taskService.completeTask(task.getId());
+        //check if email sent to users: [1,2,3]
+
+        Mockito.verify(emailNotifier).notify(Mockito.eq(task.getId()), emailsCaptor.capture());
+
+        HashSet<String> notified = new HashSet<>(emailsCaptor.getValue());
+        HashSet<String> expected = new HashSet<>(Arrays.asList("user1@wp.pl", "user2@wp.pl", "user3@wp.pl"));
+
+        Assert.assertEquals(expected, notified);
+    }
+
+}
